fa669ac1b65bb580f6134cdaf4198fb1eecdc521
Karan <karanworks16@gmail.com>
2024-04-02 18:31:07 +0530

    edit role remaning

--- src/pages/Mapping/index.js
+++ src/pages/Mapping/index.js
@@ -6,6 +6,7 @@ import {
   CardHeader,
   Col,
   Container,
+  FormFeedback,
   Input,
   Label,
   Row,
@@ -15,21 +16,21 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import RoleFormModal from "./RoleFormModal";
 import MenuList from "./MenuList";
-import DummyMenus from "../../Layouts/DummyMenuData";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const Mapping = () => {
   const [modal_list, setmodal_list] = useState(false);
   const [checkedSubmenus, setCheckedSubmenus] = useState([]);
+  const [roles, setRoles] = useState([]);
+  const [editRole, setEditRole] = useState(false);
+  const [editRoleId, setEditRoleId] = useState();
 
-  const menuDataOfUser = useSelector((state) => state.Login.user.menus);
+  const user = useSelector((state) => state.Login.user);
 
   useEffect(() => {
     checkAndSetCheckedSubmenus();
   }, []); // Run once on component mount
-
-  console.log("menu data from store ->", menuDataOfUser);
-  console.log("dummy data ->", DummyMenus);
 
   // Function to check and set checked submenus
   const checkAndSetCheckedSubmenus = () => {
@@ -38,13 +39,15 @@ const Mapping = () => {
     MenuList.forEach((menu) => {
       if (Array.isArray(menu.subItems)) {
         menu.subItems.forEach((subItem) => {
-          const existsInDummy = menuDataOfUser.some(
-            (dummyMenu) =>
-              dummyMenu.subItems &&
-              dummyMenu.subItems.some(
-                (dummySubItem) => dummySubItem.label === subItem.label
-              )
-          );
+          const existsInDummy =
+            Array.isArray(user.menus) &&
+            user.menus.some(
+              (dummyMenu) =>
+                dummyMenu.subItems &&
+                dummyMenu.subItems.some(
+                  (dummySubItem) => dummySubItem.label === subItem.label
+                )
+            );
           if (existsInDummy) {
             checkedSubmenuLabels.push(subItem.label);
           }
@@ -54,6 +57,19 @@ const Mapping = () => {
 
     setCheckedSubmenus(checkedSubmenuLabels);
   };
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/roles`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setRoles(res.data);
+      })
+      .catch((error) => {
+        console.log("error while fetching roles", error);
+      });
+  }, []);
 
   // Function to handle checkbox change
   const handleCheckboxChange = (e, menuId, subMenuId) => {
@@ -65,9 +81,6 @@ const Mapping = () => {
         prevChecked.filter((item) => item !== id)
       );
     }
-
-    console.log("menu id ->", menuId);
-    console.log("subMenu id ->", subMenuId);
   };
 
   // toggles register / edit user modal
@@ -84,16 +97,71 @@ const Mapping = () => {
       name: Yup.string().required("Please enter role name"),
     }),
     onSubmit: (values) => {
-      isEditingUser
-        ? handleUserUpdate(adminUsersData.id)
-        : handleAddUser(values);
+      editRole ? null : handleAddRole(values);
     },
   });
+
+  function handleAddRole(values) {
+    axios
+      .post(
+        `${process.env.REACT_APP_SERVER_URL}/${user.id}/role/create`,
+        values,
+        { withCredentials: true }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log("error while creating user ->", error);
+      });
+  }
+
+  // to update list of crm field when campaign is changed in select element
+  function handleRoleChange(e) {
+    roleValidation.setFieldValue("name", e.target.value);
+  }
+
+  function handleEditRole(role) {
+    setEditRole(true);
+    setmodal_list(!modal_list);
+
+    const matchingRole = roles.find((role) => role.id === e.target.value);
+
+    console.log("matching role name", matchingRole);
+
+    roleValidation.setValues({
+      name: matchingRole.name,
+    });
+  }
+
+  function handleRoleUpdate(roleId) {
+    console.log("crm fields on updation submit ->", crmFields);
+
+    axios
+      .patch(
+        `${process.env.REACT_APP_SERVER_URL}/${adminId}/role/${roleId}/edit`,
+        crmFieldValidation.values,
+        { withCredentials: true }
+      )
+      .then((res) => {
+        if (res.status === "duplicate") {
+          setCustomError(res.message);
+        } else {
+          setCrmFields(res.data);
+
+          setmodal_list(!modal_list);
+          notifyFieldUpdated();
+        }
+      })
+      .catch((err) => {
+        console.log("error while updating", err);
+      });
+  }
 
   function formHandleSubmit(e) {
     console.log("handle submit called");
     e.preventDefault();
-    validation.handleSubmit();
+    roleValidation.handleSubmit();
     return false;
   }
 
@@ -117,21 +185,41 @@ const Mapping = () => {
                       alignItems: "center",
                     }}
                   >
-                    <Input
-                      id="roleName"
-                      name="roleName"
-                      className="form-control me-1"
-                      placeholder="Select Role Name"
-                      style={{ width: "max-content" }}
-                      type="select"
-                    >
-                      <option value="" disabled>
-                        Select Role
-                      </option>
+                    <div className="me-1">
+                      <Input
+                        id="name"
+                        name="name"
+                        className="form-control"
+                        placeholder="Enter Role Name"
+                        type="select"
+                        onChange={handleRoleChange}
+                        onBlur={roleValidation.handleBlur}
+                        value={roleValidation.values.name || ""}
+                        invalid={
+                          roleValidation.touched.name &&
+                          roleValidation.errors.name
+                            ? true
+                            : false
+                        }
+                      >
+                        <option value="" disabled>
+                          Select Role Name
+                        </option>
 
-                      <option value="admin">Admin</option>
-                      <option value="manager">Manager</option>
-                    </Input>
+                        {roles?.map((role) => (
+                          <option value={role.id} key={role.id}>
+                            {role.name}
+                          </option>
+                        ))}
+                      </Input>
+
+                      {roleValidation.touched.name &&
+                      roleValidation.errors.name ? (
+                        <FormFeedback type="invalid">
+                          {roleValidation.errors.name}
+                        </FormFeedback>
+                      ) : null}
+                    </div>
 
                     <div
                       style={{
@@ -152,7 +240,7 @@ const Mapping = () => {
                         color="primary"
                         className="add-btn me-1 btn-block"
                         id="create-btn"
-                        onClick={() => tog_list()}
+                        onClick={() => handleEditRole()}
                       >
                         <i className="ri-pencil-fill"></i> Edit Role Name
                       </Button>
